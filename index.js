511fe90fca7e6b4686ad0d2fd90c915ca011148b
Anja Kammer <post@anjakammer.de>
2019-02-10 02:46:11 +0100

    wrap json in array

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const yaml = require('js-yaml')
 const fs = require('fs')
 const path = require('path')
 
+let output = '['
 fs.readdirSync(process.env.DIR).forEach(file => {
   if (path.extname(file).toLowerCase() === (process.env.EXT).toLowerCase()) {
-    console.log(
+    output = output +
       JSON.stringify(yaml.safeLoad(fs.readFileSync(process.env.DIR + '/' + file, 'utf8'))) + ','
-    )
   }
 })
+
+console.log(output + ']')
