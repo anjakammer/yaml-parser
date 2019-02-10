9a77e6b21f97ffd02432b35fe534f2558cb47918
Anja Kammer <post@anjakammer.de>
2019-02-10 03:06:16 +0100

    index by filename

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const yaml = require('js-yaml')
 const fs = require('fs')
 const path = require('path')
 
-let output = '['
+let output = '{'
 fs.readdirSync(process.env.DIR).forEach(file => {
-  if (path.extname(file).toLowerCase() === (process.env.EXT).toLowerCase()) {
-    output = output +
+  let filepath = path.parse(file)
+  if (filepath.ext.toLowerCase() === (process.env.EXT).toLowerCase()) {
+    output = output + `"${filepath.name}: "` +
       JSON.stringify(yaml.safeLoad(fs.readFileSync(process.env.DIR + '/' + file, 'utf8'))) + ','
   }
 })
-console.log(output.substring(0, output.length - 1) + ']')
+console.log(output.substring(0, output.length - 1) + '}')
